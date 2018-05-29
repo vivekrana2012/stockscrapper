46a8c4cf19d0e213d6ca437bbd7e34e26a25b3be
Vivek Rana <vivekrana.2012@gmail.com>
2018-05-29 21:09:56 +0530

    Added API Gateway route for services and working on autocomplete

--- stockscrapper-apigateway/routes/events.js
+++ stockscrapper-apigateway/routes/events.js
@@ -1,8 +1,23 @@
 var express = require('express');
+var requestHttp = require('request');
 var router = express.Router();
 
-router.get('/events', function(req, res, next){
-  res.send('Events API');
+router.get('/events', function(request, response, next){
+  response.send('Events API');
+});
+
+router.get('/events/tickers', function(request, response){
+  requestHttp('http://localhost:3001/api/events/tickers', { json:true }, function(error, responseHttp, body){
+    var result = [];
+    if (error) {
+      console.log(error);
+    }else{
+      body.forEach(function(item){
+        result.push(item['Security Id']);
+      });
+    }
+    response.send(result);
+  });
 });
 
 module.exports = router;
